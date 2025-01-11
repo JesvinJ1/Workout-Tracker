a557ed9aa844b4369c95ce6aa6bfd378c423bdc6
Jesvin Jerry <jesvinj7292@gmail.com>
2025-01-11 13:19:15 -0500

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Jesvin Jerry on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
